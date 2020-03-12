d2fc06dcff12d6c6e1f22adffb2819c248c01d05
Ivanna Pop <popivanka.21@gmail.com>
2020-03-12 14:42:56 +0200

    first commit

---
+++ ConsoleAppAnagram/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleAppAnagram
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.WriteLine("Hello again, Mykola!");
+            Console.WriteLine("Please enter the text, that should be changed ...");
+            string text = Console.ReadLine();
+
+            string[] words = text.Split(' '); 
+
+            var result = new StringBuilder(text.Length);
+
+            foreach (var word in words){
+               result.Append(ReverseOnlyLetters(new StringBuilder(word))); 
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Here is result ---> {result}"); 
+            Console.ReadLine();
+        }
+
+        static StringBuilder ReverseOnlyLetters(StringBuilder word){
+            int right = word.Length - 1;
+            int left = 0; 
+
+            while (left < right){
+                if (!char.IsLetter(word[left]))
+                    left++;
+                else if (!char.IsLetter(word[right]))
+                    right--;
+                else {
+                    char tmp = word[left];
+                    word[left] = word[right];
+                    word[right] = tmp;
+                    left++;
+                    right--;
+                }
+            }
+            return word.Append(" ");
+        }
+    }
+}
